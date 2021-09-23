f9d8918570a04204271a2e5b3cde33913bb8c206
kyler-harrison <harri828@purdue.edu>
2021-09-23 18:52:41 -0400

    load_table() determines path to load table from

--- src/gen_matrices.c
+++ src/gen_matrices.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "gen_matrices.h"
 
 /*  TODO: 
@@ -15,10 +17,41 @@
  */
 
 int main(int argc, char **argv) {
+	int mul_table[MAX_TABLE][MAX_TABLE];
+	int field = 4;  // TODO set with cl args
+
+	load_table(field, mul_table);
 
 	return 0;
 }
 
-void load_tables(int field, int mul_table[MAX_TABLE][MAX_TABLE], int div_table[MAX_TABLE][MAX_TABLE]) {
+/*
+ *  Loads in a finite field arithmetic table from one of the text files in 
+ *  the tables directory.
+ */
+
+int load_table(int field, int table[MAX_TABLE][MAX_TABLE]) {
+	char path[MAX_PATH_LEN] = "";
+
+	switch (field) {
+		case 6:
+			strncpy(path, "tables/2to6_1000011.txt", MAX_PATH_LEN);
+			break;
+		case 5:
+			strncpy(path, "tables/2to5_100101.txt", MAX_PATH_LEN);
+			break;
+		case 4:
+			strncpy(path, "tables/2to4_10011.txt", MAX_PATH_LEN);
+			break;
+		case 3:
+			strncpy(path, "tables/2to3_1011.txt", MAX_PATH_LEN);
+			break;
+		default:
+			return INVALID_FIELD;
+	}
+
+	printf("%s\n", path);
+
+	return OK;
 }
 
