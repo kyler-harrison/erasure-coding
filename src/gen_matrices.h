6c98f1c9b4f4cb76ab9cc322fd55d064b19c0171
kyler-harrison <harri828@purdue.edu>
2021-09-23 23:55:02 -0400

    removed types, dont need anymore

--- src/gen_matrices.h
+++ src/gen_matrices.h
@@ -5,10 +5,6 @@
 #define FIELD_5_DIMS (32)
 #define FIELD_6_DIMS (64)
 
-// determine which table to load in based on these
-#define MUL_TYPE (0)
-#define DIV_TYPE (1)
-
 // length of file name string 
 #define MAX_PATH_LEN (50)
 
@@ -16,6 +12,5 @@
 #define OK (0)
 #define INVALID_FIELD (-1)
 #define FILE_READ_ERR (-2)
-#define INVALID_TYPE (-3)
 
 int load_table(int field, char path[MAX_PATH_LEN], int table[MAX_TABLE][MAX_TABLE], int rows, int cols);
