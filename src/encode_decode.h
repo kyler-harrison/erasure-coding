b5eea769415e03e82653d226920eda010d7acf28
kyler-harrison <harri828@purdue.edu>
2021-10-09 22:59:58 -0400

    added params to handle_args()

--- src/encode_decode.h
+++ src/encode_decode.h
@@ -1,2 +1,2 @@
 // function prototypes
-int handle_args(int argc, char **argv);
+int handle_args(int argc, char **argv, int *op_type, char base_path[MAX_PATH_LEN], char arg_status_str[MAX_STATUS_LEN]);
