652f30d1e74c091a4013d3fd5fc63adfa882fcb1
kyler-harrison <harri828@purdue.edu>
2021-10-09 23:38:14 -0400

    encode() function prototype

--- src/encode_decode.h
+++ src/encode_decode.h
@@ -1,2 +1,3 @@
 // function prototypes
 int handle_args(int argc, char **argv, int *op_type, char file_path[MAX_PATH_LEN], char shard_base_path[MAX_PATH_LEN], char arg_status_str[MAX_STATUS_LEN]);
+int encode(char input_path[MAX_PATH_LEN], char output_base_path[MAX_PATH_LEN]);
