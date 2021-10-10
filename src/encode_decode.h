a403fd8e3079f4fd8e35d9458548001dab739238
kyler-harrison <harri828@purdue.edu>
2021-10-09 23:29:41 -0400

    added file_path arg and changed base_path to shard_base_path in handle_args()

--- src/encode_decode.h
+++ src/encode_decode.h
@@ -1,2 +1,2 @@
 // function prototypes
-int handle_args(int argc, char **argv, int *op_type, char base_path[MAX_PATH_LEN], char arg_status_str[MAX_STATUS_LEN]);
+int handle_args(int argc, char **argv, int *op_type, char file_path[MAX_PATH_LEN], char shard_base_path[MAX_PATH_LEN], char arg_status_str[MAX_STATUS_LEN]);
