36e378dea7b01d86cd618263bbd28856486c4a0e
kyler-harrison <harri828@purdue.edu>
2021-10-09 22:24:41 -0400

    constants shared between files

---
+++ src/constants.h
@@ -0,0 +1,10 @@
+#define MAX_PATH_LEN (50)  // length of file name string 
+#define MAX_STATUS_LEN (200)  // length of status message to output
+
+// status codes
+#define OK (0)
+#define INVALID_FIELD (-1)
+#define FILE_READ_ERR (-2)
+#define FILE_WRITE_ERR (-3)
+#define BAD_ARGS (-4)
+
