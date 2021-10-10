de0f9705f001038508da40d95b3af12c2c36316e
kyler-harrison <harri828@purdue.edu>
2021-10-09 22:24:05 -0400

    moved constants to constants.h

--- src/gen_matrices.h
+++ src/gen_matrices.h
@@ -5,16 +5,6 @@
 #define FIELD_5_DIMS (32)
 #define FIELD_6_DIMS (64)
 
-#define MAX_PATH_LEN (50)  // length of file name string 
-#define MAX_STATUS_LEN (200)  // length of status message to output
-
-// status codes
-#define OK (0)
-#define INVALID_FIELD (-1)
-#define FILE_READ_ERR (-2)
-#define FILE_WRITE_ERR (-3)
-#define BAD_ARGS (-4)
-
 // function prototypes
 int load_table(int field, char path[MAX_PATH_LEN], int table[MAX_TABLE][MAX_TABLE], int rows, int cols);
 int handle_args(int argc, char **argv, char arg_status[MAX_STATUS_LEN], int *k, int *n, int *overwrite, int *field);
